f5ff12b4194fc9f886c7549138728be838bd2767
RiyaGurung <bt21396@qmul.ac.uk>
2024-01-30 13:54:21 +0000

    The start of inputting another dataset and practicing the use of grep and gsub regular expressions.

--- RegularExpression.R
+++ RegularExpression.R
@@ -3,3 +3,26 @@ grep_out <- grep(pattern = '^the$', x = sentence)
 grep_out
 #the ^ and $ are used so we don't get 'they' and only 'the'
 #there is no need to use the 'print' function here
+grep_out <- grep(pattern = 'a..e', x = sentence)
+grep_out
+#a..e is looking for words that have 2 spaces between a and e
+gsub_out <- gsub(pattern = 't', x = sentence, replacement = '?')
+gsub_out
+#using the gsub function will replace your code with whatever you want 
+dung_beetles <- read.csv('dung_beetles.csv')
+#input dung beetles data, which is an integer type of data 
+str(dung_beetles)
+species_names <- colnames(dung_beetles)
+#use the colnames function like this to get all the names of the column and assign it to an object
+genus <- grep(pattern = '[C-C]', x = species_names)
+genus
+#using grep to find the species with genus names that start with 'C' 
+epithet_r <- grep(pattern = '_r', x = species_names)
+epithet_r
+#using grep to find the species that have their second name starting with the letter 'r'
+copris <- gsub(pattern = 'Copis', x = species_names, replacement = 'Copris')
+copris
+#using gsub to replace 'Copis' with 'Copris' - the correct name 
+microcopris <- gsub(pattern = 'Microcopis', x = species_names, replacement = 'Microcopris')
+microcopris
+#again using gsub to correct the species name 
